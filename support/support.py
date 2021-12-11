2a1b51aeac82e54d23c6df9805dbc0e79f2bc132
Steven Cardona <cardona.sj@gmail.com>
2021-12-10 21:33:24 -0600

    added ability to auto submit results

--- support/support.py
+++ support/support.py
@@ -2,8 +2,12 @@ from __future__ import annotations
 
 import argparse
 import contextlib
+import os
+import re
+import sys
 import time
-from urllib import error, request
+import urllib
+from urllib import error, request, parse
 from typing import Generator
 
 
@@ -24,28 +28,39 @@ def timing(name: str = '') -> Generator[None, None, None]:
         print(f'> {int(t)} {unit}{name}')
 
 
-def get_input(year: int, day: int) -> str:
+def _get_cookie_header() -> dict[str, str]:
     with open('../.env') as f:
         contents = f.read()
+    return {'Cookie': contents}
 
+
+def get_input(year: int, day: int) -> str:
     url = f'https://adventofcode.com/{year}/day/{day}/input'
-    req = request.Request(url, headers={'Cookie': contents.strip()})
+    req = request.Request(url, headers=_get_cookie_header())
     return request.urlopen(req).read().decode()
+
+
+def get_cwd_year_day() -> tuple[int, int]:
+    cwd = os.getcwd()
+    day = os.path.basename(cwd)
+    year = os.path.basename(os.path.dirname(cwd))
+    if not day.startswith('day') or not year.startswith('aoc'):
+        raise AssertionError(f'unexpected working directory: {cwd}')
+    return int(year[len('aoc'):]), int(day[len('day'):])
 
 
 def download_input() -> int:
     parser = argparse.ArgumentParser()
-    parser.add_argument('year', type=int)
-    parser.add_argument('day', type=int)
-    args = parser.parse_args()
+    parser.parse_args()
+
+    year, day = get_cwd_year_day()
 
     for _ in range(5):
         try:
-            s = get_input(args.year, args.day)
+            s = get_input(year, day)
         except error.URLError as e:
             print(f'zzz: not ready yet: {e}')
-        except Exception as e:
-            print(f'A different error occurred: {e}')
+            time.sleep(1)
         else:
             break
     else:
@@ -55,13 +70,49 @@ def download_input() -> int:
         f.write(s)
 
     lines = s.splitlines()
-    if len(lines) > 10:
-        for line in lines[:10]:
-            print(line)
-    else:
-        print(lines[0][:80])
+    print(iter(lines)) if len(lines) > 10 else print(lines[0][:80])
     print('...')
     return 0
+
+
+TOO_QUICK = re.compile('You gave an answer too recently.*tow wait.')
+WRONG = re.compile(r"That's not the right answer. *?\.")
+RIGHT = "That's the right answer"
+ALREADY_DONE = re.compile(r"You don't seem to be solving. *\?")
+
+
+def submit_solution() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--part', type=int, required=True)
+    args = parser.parse_args()
+
+    year, day = get_cwd_year_day()
+    answer = int(sys.stdin.read())
+
+    print(f'answer: {answer}')
+    params = urllib.parse.urlencode({'level': args.part, 'answer': answer})
+    req = urllib.request.Request(
+        f'https://advnetofcode.com/{year}/day/{day}/answer',
+        method='POST',
+        data=params.encode(),
+        headers=_get_cookie_header()
+    )
+    resp = urllib.request.urlopen(req)
+    contents = resp.open().decode()
+
+    for error_regex in (WRONG, TOO_QUICK, ALREADY_DONE):
+        error_match = error_regex.search(contents)
+        if error_match:
+            print(f'\033[41m{error_match[0]}\033[m')
+            return 1
+
+        if RIGHT in contents:
+            print(f'\033[42m{RIGHT}\033[m')
+            return 0
+        else:
+            # unexpected output?
+            print(contents)
+            return 1
 
 
 if __name__ == '__main__':
