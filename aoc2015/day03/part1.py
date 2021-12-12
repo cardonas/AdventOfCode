a637c30014b7ffaea5b53984c2fc0a5a4c18ca15
Steven Cardona <cardona.sj@gmail.com>
2021-12-11 23:11:44 -0600

    setup directory for 2015 day3 puzzle

---
+++ aoc2015/day03/part1.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+import argparse
+from pathlib import Path
+from typing import Optional, Union
+
+import pytest
+from support import timing
+
+INPUT_TXT = Path(__file__).parent.joinpath('input.txt')
+
+
+def compute(s: Union[list[str], str], testing: Optional[bool] = None) -> int:
+    lines = s if testing and type(s) == list[str] else s.splitlines()
+    # TODO: implement solution here!
+    return 0
+
+
+def test(input_data) -> None:
+    assert compute(input_data, testing=True) == -3
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+    with open(args.data_file) as f, timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
